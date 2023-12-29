193abea0d2faed9d3c823ccfb3e468110ee8d7ad
hungpv <phanhung2702@gmail.com>
2023-12-29 16:57:38 +0700

    [Add] login card draft

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,21 @@ export default function Login() {
   return (
     <div className="login">
       <div className='login-card'>
-        <div className='signin half'></div>
-        <div className='signup half'></div>
+      <div className="sign-in">
+          <div>Sign in</div>
+          <input placeholder="User name" type={"text"}></input>
+          <input placeholder="Password" type={"password"}></input>
+          <button className="signin-btn">Sign in</button>
+          <div>or signin with</div>
+          <div className="signin-by"></div>
+        </div>
+        <div className="approach-signup">
+          <button
+            className="approach-btn"
+          >
+            Sign up
+          </button>
+        </div>
       </div>
     </div>
   )
